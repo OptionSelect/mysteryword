6f269676ba426aaa058acdf326b158b2189ff2b2
Brandyn Sullins <woofenwauzer@gmail.com>
2017-08-03 10:45:42 -0400

    normalmode done

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const mustacheExpress = require('mustache-express')
 const expressValidator = require('express-validator')
 const words = fs.readFileSync('/usr/share/dict/words', 'utf-8').toLowerCase().split('\n')
 
-let underscores = []
-let guesses = []
-let secretword = []
-let count = 8
+var count = 8
+var guesses = []
+var underscores = []
+var secretword = []
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -24,36 +24,86 @@ let randomWord = function() {
   return words[i]
 }
 
-let newword = randomWord()
-console.log(newword)
-
-for (let j = 0; j < newword.length; j++) {
-  secretword.push(newword[j])
-}
-
-underscores = secretword.map(x => {
-  return (x = '_')
-})
-
-console.log(secretword)
-console.log(underscores)
-
 app.get('/', (req, res) => {
-  res.render('home', { underscores: underscores, guesses: guesses, count: count })
+  count = 8
+  let playing = 'Y'
+  underscores = []
+  secretword = []
+  guesses = []
+  var newword = randomWord()
+  console.log(newword)
+
+  for (let j = 0; j < newword.length; j++) {
+    secretword.push(newword[j])
+  }
+
+  underscores = secretword.map(x => {
+    return (x = '_')
+  })
+  console.log(secretword)
+  console.log(underscores)
+
+  res.render('home', { underscores: underscores, guesses: guesses, count: count, playing: playing })
 })
 
 app.post('/', (req, res) => {
-  if (secretword.includes(req.body.guess)) {
-    for (let q = 0; q < secretword.length; q++) {
-      if (secretword[q] === req.body.guess) {
-        underscores.splice(q, 1, req.body.guess)
-      }
-    }
+  let playing = 'Y'
+  req.checkBody('guess', 'You need to put guess a LETTER!.').notEmpty().isLength(0, 1).isAlpha()
+
+  var errors = req.validationErrors()
+  if (errors) {
+    res.render('home', {
+      underscores: underscores,
+      guesses: guesses,
+      count: count,
+      secretword: secretword,
+      playing: playing,
+      errors: errors
+    })
+  } else if (guesses.includes(req.body.guess)) {
+    errors = { msg: 'You already guessed that! Pick another letter!' }
+    res.render('home', {
+      underscores: underscores,
+      guesses: guesses,
+      count: count,
+      secretword: secretword,
+      playing: playing,
+      errors: errors
+    })
   } else {
-    count -= 1
-    console.log(count)
+    if (count != 0) {
+      if (secretword.includes(req.body.guess)) {
+        for (let q = 0; q < secretword.length; q++) {
+          if (secretword[q] === req.body.guess) {
+            underscores.splice(q, 1, req.body.guess)
+          }
+        }
+      } else {
+        count -= 1
+        console.log(count)
+      }
+      guesses.push(req.body.guess)
+    }
+
+    if (count === 0) {
+      let loseCondish = 'L'
+      res.render('home', {
+        underscores: underscores,
+        guesses: guesses,
+        count: count,
+        loseCondish: loseCondish,
+        secretword: secretword
+      })
+    } else if (underscores.includes('_')) {
+      res.render('home', { underscores: underscores, guesses: guesses, count: count, playing: playing })
+    } else if (underscores.includes('_') === false) {
+      let winCondish = 'W'
+      res.render('home', { underscores: underscores, guesses: guesses, count: count, winCondish: winCondish })
+    }
   }
-  guesses.push(req.body.guess)
+})
+
+app.post('/again', (req, res) => {
   res.redirect('/')
 })
 
